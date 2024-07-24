45a7fb911133ddf3bfc0db6b1f596d2a748e3bfd
bryanvengwa <bmvengwa@gmail.com>
2024-07-24 14:42:51 +0200

    added sample logos

--- src/components/ClientCard.tsx
+++ src/components/ClientCard.tsx
@@ -2,14 +2,15 @@ import Image from 'next/image';
 
 interface ClientProps {
   down? : boolean,
-  extra? : boolean
+  extra? : boolean,
+  image: string
 
 }
 
-export default function ClientCard( { down, extra } :ClientProps) {
+export default function ClientCard( { down,image , extra } :ClientProps) {
   return (
-    <div className={`flex bg-white ${down ? "lg:translate-y-[2.5em]" : "lg:translate-y-[-2.5em]"} ${extra ? "md:hidden" : ''} items-center justify-center w-[47%] md:w-[28%] lg:w-[18%] rounded-[8px] h-[13rem] shadow-md`} >
-      <Image src="/images/zinara.png" className='object-contain w-[70%] ' width={400} height={400} alt="zinara logo" />
+    <div className={`flex bg-white ${down ? "lg:translate-y-[2.5em]" : "lg:translate-y-[-2.5em]"} ${extra ? "md:hidden" : ''} items-center justify-center w-[47%] md:w-[28%] lg:w-[18%] rounded-[8px] p-2 bg-white h-[13rem] shadow-md`} >
+      <Image src={image} className='object-contain w-full h-full ' width={900} height={900} alt="partner  logo" />
     </div>
   );
 }
--- src/components/ClientSection.tsx
+++ src/components/ClientSection.tsx
@@ -6,13 +6,13 @@ export default function ClientSection() {
       <div className="container-lg  ">
       <h3 className='jost-bold lg:translate-x-[2em] text-[1.2rem] lg:text-[1.4rem]' >Meet Some Of Our Business Clients</h3>
         <div className="flex flex-wrap lg:translate-y-[3em] mt-2 space-x-[0.5em]  md:space-x-0 space-y-[0.5em] md:gap-5 lg:gap-3 items-center justify-center ">
-          <ClientCard />
-          <ClientCard down={true} />
-          <ClientCard />
-          <ClientCard down={true} />
-          <ClientCard />
+          <ClientCard image='/images/bright-logo.webp' />
+          <ClientCard image='/images/bright-logo.webp'  down={true} />
+          <ClientCard image='/images/baker-logo.webp'  />
+          <ClientCard image='/images/bright-logo.webp'  down={true} />
+          <ClientCard image='/images/mobi-logo.webp'  />
           
-          <ClientCard extra={true}   />
+          <ClientCard image='/images/bright-logo.webp'  extra={true}   />
         </div>
       </div>
     </section>
