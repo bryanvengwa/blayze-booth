1665388dfd9c63d4652062a518d3a6b39468f57b
bryanvengwa <bmvengwa@gmail.com>
2024-07-24 10:46:35 +0200

    created client ection

--- src/components/ClientCard.tsx
+++ src/components/ClientCard.tsx
@@ -1,13 +1,14 @@
 import Image from 'next/image';
 
 interface ClientProps {
-  down? : boolean
+  down? : boolean,
+  extra? : boolean
 
 }
 
-export default function ClientCard( { down } :ClientProps) {
+export default function ClientCard( { down, extra } :ClientProps) {
   return (
-    <div className={`flex bg-white ${down ? "lg:mt-[6.5rem]" : ""} items-center justify-center w-[47%] md:w-[28%] lg:w-[16rem] rounded-[8px] h-[13rem] shadow-md`} >
+    <div className={`flex bg-white ${down ? "lg:translate-y-[2.5em]" : "lg:translate-y-[-2.5em]"} ${extra ? "md:hidden" : ''} items-center justify-center w-[47%] md:w-[28%] lg:w-[18%] rounded-[8px] h-[13rem] shadow-md`} >
       <Image src="/images/zinara.png" className='object-contain w-[70%] ' width={400} height={400} alt="zinara logo" />
     </div>
   );
--- src/components/ClientSection.tsx
+++ src/components/ClientSection.tsx
@@ -2,14 +2,17 @@ import ClientCard from './ClientCard';
 
 export default function ClientSection() {
   return (
-    <section className='bg-[#F1F1F1] py-[1rem]' >
-      <div className="container-md-fluid">
-        <div className="flex flex-wrap sm:space-x-[0.5em] sm:space-y-[0.5em] md:gap-5  items-center justify-center ">
+    <section className='bg-[#F1F1F1] py-[1rem] lg:pb-[8em] lg:pt-[2em]' >
+      <div className="container-lg  ">
+      <h3 className='jost-bold lg:translate-x-[2em] text-[1.2rem] lg:text-[1.4rem]' >Meet Some Of Our Business Clients</h3>
+        <div className="flex flex-wrap lg:translate-y-[3em] mt-2 space-x-[0.5em]  md:space-x-0 space-y-[0.5em] md:gap-5 lg:gap-3 items-center justify-center ">
           <ClientCard />
           <ClientCard down={true} />
           <ClientCard />
           <ClientCard down={true} />
           <ClientCard />
+          
+          <ClientCard extra={true}   />
         </div>
       </div>
     </section>
