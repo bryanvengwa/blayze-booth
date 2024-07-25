2c5f5b4cc5a0a1c48737be70392a892835212c30
bryanvengwa <bmvengwa@gmail.com>
2024-07-25 15:47:19 +0200

    removed error from client card

--- src/components/ClientCard.tsx
+++ src/components/ClientCard.tsx
@@ -10,7 +10,7 @@ interface ClientProps {
 export default function ClientCard( { down,image , extra } :ClientProps) {
   return (
     <div className={`flex bg-white ${down ? "lg:translate-y-[2.5em]" : "lg:translate-y-[-2.5em]"} ${extra ? "md:hidden" : ''} items-center justify-center w-[47%] md:w-[28%] lg:w-[18%] rounded-[8px] p-2 bg-white h-[13rem] shadow-md`} >
-      <Image src={image} className='object-containb w-full h-full ' width={900} height={900} alt="partner  logo" />
+      <Image src={image} className='object-contain w-full h-full ' width={900} height={900} alt="partner  logo" />
     </div>
   );
 }
